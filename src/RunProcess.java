05c6337eef54b71061e469bc9ac7995be84b8728
Daniel Vera <44836822+DanielVera33@users.noreply.github.com>
2019-10-03 16:03:56 +0200

    Update RunProcess.java

--- src/RunProcess.java
+++ src/RunProcess.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Arrays;
 
-public class RunProcess {
+public class RunProcess { // DANIEL VERA MARTINEZ
 	public static void main(String[] args) throws IOException {
 		if (args.length <= 0) {
 			System.err.println("Se necesita un programa a ejecutar");
@@ -11,13 +11,32 @@ public class RunProcess {
 		try {
 			Process process = pb.start();
 			int retorno = process.waitFor();
-			System.out.println("La ejecuciÛn de " + Arrays.toString(args) + " devuelve " + retorno);
+			System.out.println("La ejecuci√≥n de " + Arrays.toString(args) + " devuelve " + retorno);
 		} catch (IOException ex) {
-			System.err.println("ExcepciÛn de E/S!!");
+			System.err.println("Excepci√≥n de E/S!!");
 			System.exit(-1);
 		} catch (InterruptedException ex) {
-			System.err.println("El proceso hijo finalizÛ de forma incorrecta");
+			System.err.println("El proceso hijo finaliz√≥ de forma incorrecta");
 			System.exit(-1);
 		}
 	}
 }
+
+	// a) He ejecutado por primera vez el programa y me ha pedido por consola escribir un programa,
+	// He seleccionado Run Configurations y dentro de este la pestana Arguments,
+	// En el espacio de escritura de Program arguments he escrito calc.exe y lo he ejecutado con run.
+
+	// b) Al ejecutar sin poner argumentos lo que sucede es que no se ejecuta y te pide por pantalla,
+	// Que se necesita un programa para ejecutar, se cumple el primer if, printeando un texto como error en color rojo
+
+	// c) He escrito en los argumentos notepad.exe y separado por un espacio hola,
+	// Al ejecutarlo lo que ha sucedido es que abrio el programa pero me salio un aviso ya que no existe,
+	// ningun archivo .txt de nombre hola, probando con un archivo existente abre ese archivo del programa seleccionado.
+
+	// d) He creado un bucle infinito que permite que se ejecute una detras de otra pero debe ser parado rapido ya que si no colapsa el ordenador.
+	// int a=0;
+	//for (int i = 1; i > a; i++) {
+	//Process process = pb.start();
+	//int retorno = process.waitFor();
+	//System.out.println("La ejecuci√≥n de " + Arrays.toString(args) + "devuelve " + retorno);
+	//}
