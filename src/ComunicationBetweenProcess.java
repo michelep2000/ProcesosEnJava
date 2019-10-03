df79bd702dd19bd4d39bafe689baab58dd694df5
Daniel Vera <44836822+DanielVera33@users.noreply.github.com>
2019-10-03 16:21:41 +0200

    Update ComunicationBetweenProcess.java

--- src/ComunicationBetweenProcess.java
+++ src/ComunicationBetweenProcess.java
@@ -5,15 +5,25 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class ComunicationBetweenProcess {
-	public static void main(String args[]) throws IOException {
-		Process process = new ProcessBuilder(args).start();
-		InputStream is = process.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
-		String line;
-		System.out.println("Salida del proceso " + Arrays.toString(args) + ":");
-		while ((line = br.readLine()) != null) {
-			System.out.println(line);
+	public static void main(String args[]) {
+		try { // C) Creo el Try
+			Process process = new ProcessBuilder(args).start();
+			InputStream is = process.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is, "UTF-8"); // B) Escribo UTF-8
+			BufferedReader br = new BufferedReader(isr);
+			String line;
+			System.out.println("Salida del proceso " + Arrays.toString(args) + ":");
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			is.close(); // C) Cierro is InputStream is
+		} catch (Exception e) { // C) Capta la exception 
+			e.printStackTrace();
 		}
 	}
 }
+
+// A) Se obtiene lo siguiente:
+// Salida del proceso [cmd.exe]:
+// Microsoft Windows [Versi¢n 10.0.18362.356]
+// (c) 2019 Microsoft Corporation. Todos los derechos reservados.
